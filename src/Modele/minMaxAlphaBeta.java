6929708786e07544bb5f334bd72e31664475ab1f
dbJoker <tri-23-sc@hotmail.ca>
2017-11-15 14:05:48 -0500

    squelette de alpha beta

---
+++ src/Modele/minMaxAlphaBeta.java
@@ -0,0 +1,62 @@
+package Modele;
+
+public class minMaxAlphaBeta {
+
+    private int score = 0;
+    private int MaxScore = 0;
+
+    public int minMaxAlphaBeta(int position, String joueur, int alpha, int beta){  // player may be "computer" or "opponent"
+       int alphaTmp;
+       int betaTmp;
+        if (gameover || position == 0) {
+            return score;
+        }
+        //si c<est notre tour
+        if(joueur == "Max") {
+            alphaTmp = -1;
+            //pour tout les enfant de position
+            for(int i=0;i < 0; i++) {
+                score = minMaxAlphaBeta(position+i,"Min",Math.max(alpha, alphaTmp),beta);
+                alphaTmp = Math.max(maxScore, score);
+                if(alphaTmp >= beta) {
+                    return alphaTmp;
+                }
+            }
+            return alphaTmp;
+
+        }else if(joueur == "Min"){
+            betaTmp = 101;
+            //pour tout les enfant de position
+            for(int i=0;i < 0; i++) {
+                score = minMaxAlphaBeta(position+i,"Min",alpha,Math.min(beta,betaTmp));
+                betaTmp = Math.min(maxScore, score);
+                if(betaTmp < beta) {
+                    return alphaTmp;
+                }
+            }
+            return alphaTmp;
+        }
+
+
+        children = all valid moves for this "player"
+        if (player is computer, i.e., max's turn){
+        // Find max and store in alpha
+        for each child {
+            score = minimax(level - 1, opponent, alpha, beta)
+            if (score > alpha) alpha = score
+            if (alpha >= beta) break;  // beta cut-off
+        }
+        return alpha
+    } else (player is opponent, i.e., min's turn)
+            // Find min and store in beta
+            for each child {
+        score = minimax(level - 1, computer, alpha, beta)
+        if (score < beta) beta = score
+        if (alpha >= beta) break;  // alpha cut-off
+    }
+       return beta
+}
+}
+
+
+        }
