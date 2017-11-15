8f5dc50d5a9a45927621b3a2864b04a37c94cb98
dbJoker <tri-23-sc@hotmail.ca>
2017-11-15 14:12:59 -0500

    small fix

--- src/Modele/minMaxAlphaBeta.java
+++ src/Modele/minMaxAlphaBeta.java
@@ -3,42 +3,43 @@ package Modele;
 public class minMaxAlphaBeta {
 
     private int score = 0;
-    private int MaxScore = 0;
 
-    public int minMaxAlphaBeta(int position, String joueur, int alpha, int beta){  // player may be "computer" or "opponent"
-       int alphaTmp;
-       int betaTmp;
-        if (gameover || position == 0) {
+    public int minMaxAlphaBeta(int position, String joueur, int alpha, int beta) {  // player may be "computer" or "opponent"
+        int alphaTmp;
+        int betaTmp;
+        int maxScore = 0;
+        if (position == 99999 || position == 0) {
             return score;
         }
         //si c<est notre tour
-        if(joueur == "Max") {
+        if (joueur == "Max") {
             alphaTmp = -1;
             //pour tout les enfant de position
-            for(int i=0;i < 0; i++) {
-                score = minMaxAlphaBeta(position+i,"Min",Math.max(alpha, alphaTmp),beta);
+            for (int i = 0; i < 0; i++) {
+                score = minMaxAlphaBeta(position + i, "Min", Math.max(alpha, alphaTmp), beta);
                 alphaTmp = Math.max(maxScore, score);
-                if(alphaTmp >= beta) {
+                if (alphaTmp >= beta) {
                     return alphaTmp;
                 }
             }
             return alphaTmp;
 
-        }else if(joueur == "Min"){
+        } else if (joueur == "Min") {
             betaTmp = 101;
             //pour tout les enfant de position
-            for(int i=0;i < 0; i++) {
-                score = minMaxAlphaBeta(position+i,"Min",alpha,Math.min(beta,betaTmp));
+            for (int i = 0; i < 0; i++) {
+                score = minMaxAlphaBeta(position + i, "Min", alpha, Math.min(beta, betaTmp));
                 betaTmp = Math.min(maxScore, score);
-                if(betaTmp < beta) {
-                    return alphaTmp;
+                if (betaTmp < beta) {
+                    return betaTmp;
                 }
             }
-            return alphaTmp;
+            return betaTmp;
         }
+        return 0;
 
 
-        children = all valid moves for this "player"
+/*        children = all valid moves for this "player"
         if (player is computer, i.e., max's turn){
         // Find max and store in alpha
         for each child {
@@ -55,8 +56,6 @@ public class minMaxAlphaBeta {
         if (alpha >= beta) break;  // alpha cut-off
     }
        return beta
+}*/
+    }
 }
-}
-
-
-        }
