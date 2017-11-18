906c00bee6ce986a0b9df447fb7a76a105068080
dbJoker <tri-23-sc@hotmail.ca>
2017-11-18 12:27:04 -0500

    bonne lecture des mouvement.

--- src/Modele/Board.java
+++ src/Modele/Board.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import java.util.Random;
 
 public class Board {
-    private Map<Integer, Character> map = new HashMap<Integer, Character>();
+    private Map<String, Integer> map = new HashMap<String, Integer>();
+    private Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
     private int[][] board = null;
     private final int BOARD_SIZE = 13;
     private Random rn = new Random();
@@ -68,11 +69,23 @@ public class Board {
         String strMoveDepart = "";
         String strMoveArivee = "";
         // todo fix this
+        /*
         int rangeeDepart = Integer.parseInt(move.substring(0,move.indexOf('-')).substring(0,1));
         int colonneDepart = Integer.parseInt(move.substring(0,move.indexOf('-')).substring(1,2));
 
         int rangeeArrive = Integer.parseInt( move.substring(move.indexOf('-')+1, move.length()).substring(0,1));
         int colonneFin = Integer.parseInt( move.substring(move.indexOf('-')+1, move.length()).substring(1,2));
+*/
+
+        initMap();
+       strMoveDepart = move.substring(move.indexOf('-')+2, move.length()).substring(0,1);
+        strMoveDepart = move.substring(move.indexOf('-')+2, move.length()).substring(1,2);
+
+        int rangeeDepart = map.get(move.substring(0,move.indexOf('-')).substring(0,1));
+        int colonneDepart = map2.get(Integer.parseInt(move.substring(0,move.indexOf('-')).substring(1,2)));
+
+        int rangeeArrive = map.get(move.substring(move.indexOf('-')+2, move.length()).substring(0,1));
+        int colonneFin = map2.get(Integer.parseInt( move.substring(move.indexOf('-')+2, move.length()).substring(1,2)));
 
         int[][] depart = new int[rangeeDepart][colonneDepart];
         int[][] arrivee = new int[rangeeArrive][colonneFin];
@@ -81,6 +94,40 @@ public class Board {
         strMoveDepart = move.substring(0, move.indexOf('-'));
 
         strMoveArivee = move.substring(move.indexOf('-')+1, move.length());
+
+
+    }
+
+    public void initMap(){
+        this.map.put("A",0);
+        this.map.put("B",1);
+        this.map.put("C",2);
+        this.map.put("D",3);
+        this.map.put("E",4);
+        this.map.put("F",5);
+        this.map.put("G",6);
+        this.map.put("H",7);
+        this.map.put("I",8);
+        this.map.put("J",9);
+        this.map.put("K",10);
+        this.map.put("L",11);
+        this.map.put("M",12);
+
+
+        this.map2.put(13,0);
+        this.map2.put(12,1);
+        this.map2.put(11,2);
+        this.map2.put(10,3);
+        this.map2.put(9,4);
+        this.map2.put(8,5);
+        this.map2.put(7,6);
+        this.map2.put(6,7);
+        this.map2.put(5,8);
+        this.map2.put(4,9);
+        this.map2.put(3,10);
+        this.map2.put(2,11);
+        this.map2.put(1,12);
+
 
 
     }
